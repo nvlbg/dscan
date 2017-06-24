40d0a955a58b79efbd23f33bc9454fdee6ab2083
Nikolai Lazarov <nikolai.lazarov96@gmail.com>
2017-06-24 09:08:45 +0300

    Adds a struct representing a network

---
+++ apps/server/lib/server/network.ex
@@ -0,0 +1,70 @@
+defmodule Network do
+  defstruct [:first_ip, :last_ip, :mask]
+
+  @ip_mask ~r/^((?:\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])\.
+                (?:\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])\.
+                (?:\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])\.
+                (?:\d{1,2}|1\d{2}|2[0-4]\d|25[0-5]))
+                (?:\/(\d|[12]\d|3[0-2]))?$/x
+
+  def new(network) do
+    case Regex.run(@ip_mask, network) do
+      [_, ip] ->
+        ip_binary = ip_to_binary(ip)
+        %Network{first_ip: ip_binary, last_ip: ip_binary, mask: 32}
+      [_, ip, mask] ->
+        mask = mask |> String.to_integer
+        {first, last} = ip_to_binary(ip, mask)
+        %Network{first_ip: first, last_ip: last, mask: mask}
+    end
+  end
+
+  defp ip_to_binary(ip) do
+    ip
+    |> String.split(".")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce(<<>>, fn octet, acc -> acc <> << octet >> end)
+  end
+
+  defp ip_to_binary(ip, mask) do
+    host = 32 - mask
+    << net::size(mask), _::size(host) >> = ip_to_binary(ip)
+    first_ip = << net::size(mask),  0::size(host) >>
+    last_ip  = << net::size(mask), -1::size(host) >>
+    {first_ip, last_ip}
+  end
+
+  def partition(%Network{first_ip: first, last_ip: last, mask: mask}, n) do
+    host = 32 - mask
+    << net::size(mask),  lower::size(host) >> = first
+    << ^net::size(mask), upper::size(host) >> = last
+
+    n = min(n, upper - lower + 1)
+
+    interval = div(upper - lower + 1, n)
+    remaining = rem(upper - lower + 1, n)
+
+    subnetworks = if remaining > 0 do
+      0..remaining-1
+      |> Stream.map(fn x ->
+        first_ip = << net::size(mask), (lower + x * (interval + 1))::size(host) >>
+        last_ip  = << net::size(mask), (lower + (x + 1) * (interval + 1) - 1)::size(host) >>
+        %Network{first_ip: first_ip, last_ip: last_ip, mask: mask}
+      end)
+      |> Enum.into([])
+    else
+      []
+    end
+
+    remaining..n-1
+    |> Stream.map(fn x ->
+      first_ip = << net::size(mask),
+                    (lower + remaining * (interval + 1) + (x - remaining) * interval)::size(host) >>
+      last_ip  = << net::size(mask),
+                    (lower + remaining * (interval + 1) + (x + 1 - remaining) * interval - 1)::size(host) >>
+      %Network{first_ip: first_ip, last_ip: last_ip, mask: mask}
+    end)
+    |> Enum.into(subnetworks)
+  end
+end
+
---
+++ apps/server/test/network_test.exs
@@ -0,0 +1,91 @@
+defmodule NetworkTest do
+  use ExUnit.Case
+
+  test "Can create network" do
+    net = Network.new("192.168.1.1/24")
+
+    assert net.first_ip == << 192, 168, 1, 0 >>
+    assert net.last_ip == << 192, 168, 1, 255 >>
+    assert net.mask == 24
+  end
+
+  test "Can create a single ip network" do
+    net = Network.new("8.8.8.8")
+
+    assert net.first_ip == << 8, 8, 8, 8 >>
+    assert net.last_ip == << 8, 8, 8, 8 >>
+    assert net.mask == 32
+  end
+
+  test "Can partition network" do
+    net = Network.new("192.168.1.1/24")
+
+    [first, second] = Network.partition(net, 2)
+    
+    assert first.first_ip == << 192, 168, 1, 0 >>
+    assert first.last_ip == << 192, 168, 1, 127 >>
+    assert first.mask == 24
+
+    assert second.first_ip == << 192, 168, 1, 128 >>
+    assert second.last_ip == << 192, 168, 1, 255 >>
+    assert second.mask == 24
+  end
+
+  test "Single ip is not partitioned" do
+    ip = Network.new("1.2.3.4")
+
+    assert Network.partition(ip, 1) == [ip]
+    assert Network.partition(ip, 2) == [ip]
+    assert Network.partition(ip, 100) == [ip]
+  end
+
+  test "Partitioning works when network is not exact multiple" do
+    net = Network.new("1.2.3.4/29")
+
+    [first, second, third] = Network.partition(net, 3)
+
+    assert first.first_ip == << 1, 2, 3, 0 >>
+    assert first.last_ip == << 1, 2, 3, 2 >>
+    assert first.mask == 29
+
+    assert second.first_ip == << 1, 2, 3, 3 >>
+    assert second.last_ip == << 1, 2, 3, 5 >>
+    assert second.mask == 29
+
+    assert third.first_ip == << 1, 2, 3, 6 >>
+    assert third.last_ip == << 1, 2, 3, 7 >>
+    assert third.mask == 29
+
+    [first, second, third, fourth, fifth] = \
+      Network.partition(net, 5)
+
+    assert first.first_ip == << 1, 2, 3, 0 >>
+    assert first.last_ip == << 1, 2, 3, 1 >>
+    assert first.mask == 29
+
+    assert second.first_ip == << 1, 2, 3, 2 >>
+    assert second.last_ip == << 1, 2, 3, 3 >>
+    assert second.mask == 29
+
+    assert third.first_ip == << 1, 2, 3, 4 >>
+    assert third.last_ip == << 1, 2, 3, 5 >>
+    assert third.mask == 29
+
+    assert fourth.first_ip == << 1, 2, 3, 6 >>
+    assert fourth.last_ip == << 1, 2, 3, 6 >>
+    assert fourth.mask == 29
+
+    assert fifth.first_ip == << 1, 2, 3, 7 >>
+    assert fifth.last_ip == << 1, 2, 3, 7 >>
+    assert fifth.mask == 29
+  end
+
+  test "Partitioning into more subnets than available ips" do
+    net = Network.new("8.8.8.8/28")
+
+    assert Enum.count(Network.partition(net, 32)) == 16
+    assert Enum.count(Network.partition(net, 17)) == 16
+    assert Enum.count(Network.partition(net, 16)) == 16
+  end
+end
+
